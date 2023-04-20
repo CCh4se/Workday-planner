fc0ef25be59fc9712949616a869730df84431bc2
Chase Miller <chasehmiller97@gmail.com>
2023-04-19 20:47:05 -0400

    finished time blocks

--- script.js
+++ script.js
@@ -19,14 +19,14 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
 
-  // current day display
+  // Current day display
   var currentDayEl = $('#currentDay');
   console.log(currentDayEl);
   
   var CurrentDay = dayjs().format('dddd MMMM, D');
   currentDayEl.text(CurrentDay);
 
-  // past, present, future classes
+  // Create past, present, and future classes via military time. But still display normally on the website.
   var currentHour = dayjs().hour();
   console.log(currentHour);
 
@@ -34,16 +34,173 @@ $(function () {
   timeNine.addClass('');
 
   if(9 < currentHour) {
-    console.log("past")
+    console.log("past");
     timeNine.addClass('past');
   }
   if(9 === currentHour) {
-    console.log("present")
+    console.log("present");
     timeNine.addClass('present');
 
   }
-  if (9 > currentHour){
-    console.log("future")
+  if(9 > currentHour){
+    console.log("future");
     timeNine.addClass('future');
   }
+
+  var timeTen = $("#hour-10");
+  timeTen.addClass('');
+
+  if(10 < currentHour) {
+    console.log("past");
+    timeTen.addClass('past');
+  }
+  if(10 === currentHour) {
+    console.log("present");
+    timeTen.addClass('present');
+
+  }
+  if(10 > currentHour){
+    console.log("future");
+    timeTen.addClass('future');
+  }
+
+  var timeEleven = $("#hour-11");
+  timeEleven.addClass('');
+
+  if(11 < currentHour) {
+    console.log("past");
+    timeEleven.addClass('past');
+  }
+  if(11 === currentHour) {
+    console.log("present");
+    timeEleven.addClass('present');
+
+  }
+  if(11 > currentHour){
+    console.log("future");
+    timeEleven.addClass('future');
+  }
+
+  var timeTwelve = $("#hour-12");
+  timeTwelve.addClass('');
+
+  if(12 < currentHour) {
+    console.log("past");
+    timeTwelve.addClass('past');
+  }
+  if(12 === currentHour) {
+    console.log("present");
+    timeTwelve.addClass('present');
+
+  }
+  if(12 > currentHour){
+    console.log("future");
+    timeTwelve.addClass('future');
+  }
+
+  var timeThirteen = $("#hour-13");
+  timeThirteen.addClass('');
+
+  if(1 < currentHour) {
+    console.log("past");
+    timeThirteen.addClass('past');
+  }
+  if(1 === currentHour) {
+    console.log("present");
+    timeThirteen.addClass('present');
+
+  }
+  if(1 > currentHour){
+    console.log("future");
+    timeThirteen.addClass('future');
+  }
+
+  var timeFourteen = $("#hour-14");
+  timeFourteen.addClass('');
+
+  if(2 < currentHour) {
+    console.log("past");
+    timeFourteen.addClass('past');
+  }
+  if(2 === currentHour) {
+    console.log("present");
+    timeFourteen.addClass('present');
+
+  }
+  if(2 > currentHour){
+    console.log("future");
+    timeFourteen.addClass('future');
+  }
+
+  var timeFifteen = $("#hour-15");
+  timeFifteen.addClass('');
+
+  if(3 < currentHour) {
+    console.log("past");
+    timeFifteen.addClass('past');
+  }
+  if(3 === currentHour) {
+    console.log("present");
+    timeFifteen.addClass('present');
+
+  }
+  if(3 > currentHour){
+    console.log("future");
+    timeFifteen.addClass('future');
+  }
+
+  var timeSixteen = $("#hour-16");
+  timeSixteen.addClass('');
+
+  if(4 < currentHour) {
+    console.log("past");
+    timeSixteen.addClass('past');
+  }
+  if(4 === currentHour) {
+    console.log("present");
+    timeSixteen.addClass('present');
+
+  }
+  if(4 > currentHour){
+    console.log("future");
+    timeSixteen.addClass('future');
+  }
+
+  var timeSeventeen = $("#hour-17");
+  timeSeventeen.addClass('');
+
+  if(5 < currentHour) {
+    console.log("past");
+    timeSeventeen.addClass('past');
+  }
+  if(5 === currentHour) {
+    console.log("present");
+    timeSeventeen.addClass('present');
+
+  }
+  if(5 > currentHour){
+    console.log("future");
+    timeSeventeen.addClass('future');
+  }
+
+  var timeEightteen = $("#hour-18");
+  timeEightteen.addClass('');
+
+  if(6 < currentHour) {
+    console.log("past");
+    timeEightteen.addClass('past');
+  }
+  if(6 === currentHour) {
+    console.log("present");
+    timeEightteen.addClass('present');
+
+  }
+  if(6 > currentHour){
+    console.log("future");
+    timeEightteen.addClass('future');
+  }
 });
+
+// local storage saved 
+
+
